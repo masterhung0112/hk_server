c2ee3245dd0c3d1d2ea69b2bc6f839c9420ec2b2
HungBN <hungbn0112@gmail.com>
2020-07-22 21:33:49 +0700

    Add newLogger

--- go.mod
+++ go.mod
@@ -15,5 +15,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1
 	go.uber.org/zap v1.15.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
--- mlog/log.go
+++ mlog/log.go
@@ -1,11 +1,24 @@
 package mlog
 
 import (
+  "os"
   "context"
   "go.uber.org/zap"
   "go.uber.org/zap/zapcore"
 
   "github.com/mattermost/logr"
+  "gopkg.in/natefinch/lumberjack.v2"
+)
+
+const (
+  // Very verbose messages for debugging specific issues
+  LevelDebug = "debug"
+  // Default log level, informational
+  LevelInfo = "info"
+  // Warnings are messages about possible issues
+  LevelWarn = "warn"
+  // Errors are messages about things we know are problems
+  LevelError = "error"
 )
 
 type Field = zapcore.Field
@@ -134,4 +147,60 @@ func (l *Logger) ConfigAdvancedLogging(targets LogTargetCfg) error {
 //config source.
 func (l *Logger) AddTarget(target logr.Target) error {
   return l.logrLogger.Logr().AddTarget(target)
+}
+
+func getZapLevel(level string) zapcore.Level {
+  switch level {
+  case LevelInfo:
+    return zapcore.InfoLevel
+  case LevelWarn:
+    return zapcore.WarnLevel
+  case LevelDebug:
+    return zapcore.DebugLevel
+  case LevelError:
+    return zapcore.ErrorLevel
+  default:
+    return zapcore.InfoLevel
+  }
+}
+
+func makeEncoder(json bool) zapcore.Encoder {
+  encoderConfig := zap.NewProductionEncoderConfig()
+  if json {
+    return zapcore.NewJSONEncoder(encoderConfig)
+  }
+
+  encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+  return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func NewLogger(config *LoggerConfiguration) *Logger {
+  cores := []zapcore.Core{}
+  logger := &Logger{
+    consoleLevel: zap.NewAtomicLevelAt(getZapLevel(config.ConsoleLevel)),
+    fileLevel:    zap.NewAtomicLevelAt(getZapLevel(config.FileLevel)),
+  }
+
+  if config.EnableConsole {
+    writer := zapcore.Lock(os.Stderr)
+    core := zapcore.NewCore(makeEncoder(config.ConsoleJson), writer, logger.consoleLevel)
+    cores = append(cores, core)
+  }
+
+  if config.EnableFile {
+    writer := zapcore.AddSync(&lumberjack.Logger{
+      Filename: config.FileLocation,
+      MaxSize:  100,
+      Compress: true,
+    })
+    core := zapcore.NewCore(makeEncoder(config.FileJson), writer, logger.fileLevel)
+    cores = append(cores, core)
+  }
+
+  combinedCore := zapcore.NewTee(cores...)
+
+  logger.zap = zap.New(combinedCore,
+    zap.AddCaller(),
+  )
+  return logger
 }
