08a2e14bf4cc93f5e6a36a8ffaa92b58b4b5fcfc
hung <hungbn0112@gmail.com>
2020-07-22 11:48:30 +0700

    Add watcher

--- config/watcher.go
+++ config/watcher.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"github.com/masterhung0112/go_server/mlog"
+  "path/filepath"
   "github.com/pkg/errors"
   "github.com/fsnotify/fsnotify"
 
@@ -20,11 +22,48 @@ func newWatcher(path string, callback func()) (*watcher, error) {
     return nil, errors.Wrapf(err, "Failed to create fs notify watcher for %s", path)
   }
 
+  path = filepath.Clean(path)
+
+  // Watch the entire containing directory
+  configDir, _ := filepath.Split(path)
+  if err := fsWatcher.Add(configDir); err != nil {
+    if closeErr := fsWatcher.Close(); closeErr != nil {
+      mlog.Error("Failed to stop fsnotify watcher for %s", mlog.String("path", path), mlog.Err(closeErr))
+    }
+    return nil, errors.Wrapf(err, "Failed to watch directory %s", configDir)
+  }
+
   w := &watcher{
     fsWatcher:  fsWatcher,
     close:      make(chan struct{}),
     closed:     make(chan struct{}),
   }
+
+  go func() {
+    defer close(w.closed)
+    defer func() {
+      if err := fsWatcher.Close(); err != nil {
+        mlog.Error("failed to stop fsnotify watcher for %s", mlog.String("path", path))
+      }
+    }()
+
+    for {
+      select {
+      case event := <-fsWatcher.Events:
+        // We only care about the given file.
+        if filepath.Clean(event.Name) == path {
+          if event.Op & fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
+            mlog.Info("Config file watcher detected a change", mlog.String("path", path))
+            go callback()
+          }
+        }
+      case err := <-fsWatcher.Errors:
+        mlog.Error("Failed while watching config file", mlog.String("path", path), mlog.Err(err))
+      case <-w.close:
+        return
+      }
+    }
+  }()
 
   return w, nil
 }
