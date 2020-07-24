d2287fb9ffe370b3c61e16e8ea4b507b1848f424
hung <hungbn0112@gmail.com>
2020-07-24 16:26:54 +0700

    Add build/Dockerfile, add http test in apitestlib

--- api/apitestlib.go
+++ api/apitestlib.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"net/http"
+	"github.com/stretchr/testify/require"
+	"strings"
 	"fmt"
 	"testing"
 	"github.com/masterhung0112/go_server/config"
@@ -48,4 +51,35 @@ func (th *TestHelper) CreateClient() *model.Client {
 
 func Setup(tb testing.TB) *TestHelper {
   return setupTestHelper()
+}
+
+func (th *TestHelper) GenerateTestEmail() string {
+  return strings.ToLower(model.NewId() + "@localhost")
+}
+
+func (th *TestHelper) GenerateTestUsername() string {
+	return "fakeuser" + model.NewRandomString(10)
+}
+
+func checkHTTPStatus(t *testing.T, resp *model.Response, expectedStatus int, expectError bool) {
+	t.Helper()
+
+	require.NotNilf(t, resp, "Unexpected nil response, expected http:%v, expectError:%v", expectedStatus, expectError)
+	if expectError {
+		require.NotNil(t, resp.Error, "Expected a non-nil error and http status:%v, got nil, %v", expectedStatus, resp.StatusCode)
+	} else {
+		require.Nil(t, resp.Error, "Expected no error and http status:%v, got %q, http:%v", expectedStatus, resp.Error, resp.StatusCode)
+	}
+	require.Equalf(t, expectedStatus, resp.StatusCode, "Expected http status:%v, got %v (err: %q)", expectedStatus, resp.StatusCode, resp.Error)
+}
+
+func CheckNoError(t *testing.T, resp *model.Response) {
+  t.Helper()
+
+  require.Nil(t, resp.Error, "expected no error")
+}
+
+func CheckCreatedStatus(t *testing.T, resp *model.Response) {
+	t.Helper()
+	checkHTTPStatus(t, resp, http.StatusCreated, false)
 }
--- api/user_test.go
+++ api/user_test.go
@@ -4,13 +4,13 @@ import (
 	"github.com/stretchr/testify/require"
   "testing"
 
-  // "github.com/masterhung0112/go_server/model"
+  "github.com/masterhung0112/go_server/model"
 )
 
 func TestConnectUserGet(t *testing.T) {
   th := Setup(t)
 
-  r, err := th.Client.DoApiGet("/articles/users/", "")
+  _, err := th.Client.DoApiGet("/users", "")
   if err != nil {
     t.Fatal(err.Error())
   }
@@ -18,14 +18,21 @@ func TestConnectUserGet(t *testing.T) {
 
 
 func TestCreateUser(t *testing.T) {
+  th := Setup(t)
 
-  // user := model.User{
-  //   Id:        "",
-  //   UserName:  "",
-  //   Password:  "",
-  //   Email:     "",
-  //   Roles:     model.SYSTEM_ADMIN_ROLE_ID,
-  // }
+  user := model.User{
+    UserName:  th.GenerateTestUsername(),
+    Password:  "test",
+    Email:     th.GenerateTestEmail(),
+    Roles:     model.SYSTEM_ADMIN_ROLE_ID,
+  }
 
+  ruser, resp := th.Client.CreateUser(&user)
+  require.NotNil(t, ruser)
+  CheckNoError(t, resp)
+  CheckCreatedStatus(t, resp)
+
+  // require.Equal(t, user.Nickname, ruser.Nickname, "nickname didn't match")
 
 }
+
