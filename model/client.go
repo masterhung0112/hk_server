6911d6a89334bd315031ece6016f9396e8e0495c
hungbn <masterhung0112@gmail.com>
2020-11-20 22:32:17 +0700

    Update config

--- model/client.go
+++ model/client.go
@@ -9,29 +9,28 @@ import (
 )
 
 const (
-	HEADER_REQUEST_ID = "X-Request-ID"
-	HEADER_VERSION_ID = "X-Version-ID"
-	HEADER_CLUSTER_ID = "X-Cluster-ID"
+  HEADER_REQUEST_ID         = "X-Request-ID"
+	HEADER_VERSION_ID         = "X-Version-ID"
+	HEADER_CLUSTER_ID         = "X-Cluster-ID"
+	HEADER_ETAG_SERVER        = "ETag"
+	HEADER_ETAG_CLIENT        = "If-None-Match"
+	HEADER_FORWARDED          = "X-Forwarded-For"
+	HEADER_REAL_IP            = "X-Real-IP"
+	HEADER_FORWARDED_PROTO    = "X-Forwarded-Proto"
+	HEADER_TOKEN              = "token"
+	HEADER_CSRF_TOKEN         = "X-CSRF-Token"
+	HEADER_BEARER             = "BEARER"
+	HEADER_AUTH               = "Authorization"
+	HEADER_REQUESTED_WITH     = "X-Requested-With"
+  HEADER_REQUESTED_WITH_XML = "XMLHttpRequest"
 
-	HEADER_ETAG_SERVER = "ETag"
-	HEADER_ETAG_CLIENT = "If-None-Match"
-
-	HEADER_TOKEN  = "token"
-	HEADER_BEARER = "BEARER"
-	HEADER_AUTH   = "Authorization"
-
-	STATUS           = "status"
-	STATUS_OK        = "OK"
-	STATUS_FAIL      = "FAIL"
-	STATUS_UNHEALTHY = "UNHEALTHY"
-	STATUS_REMOVE    = "REMOVE"
+	STATUS                    = "status"
+	STATUS_OK                 = "OK"
+	STATUS_FAIL               = "FAIL"
+	STATUS_UNHEALTHY          = "UNHEALTHY"
+	STATUS_REMOVE             = "REMOVE"
 
 	API_URL_SUFFIX = "/api/v1"
-
-	HEADER_REQUESTED_WITH     = "X-Requested-With"
-	HEADER_REQUESTED_WITH_XML = "XMLHttpRequest"
-
-	HEADER_FORWARDED_PROTO = "X-Forwarded-Proto"
 )
 
 type Response struct {
