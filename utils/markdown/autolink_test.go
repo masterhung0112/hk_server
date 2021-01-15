af6960d29d34500a81ced9cb925de118612f9038
hung <hungbn0112@gmail.com>
2021-01-15 11:09:14 +0700

    Update mmuser to hkuser

--- utils/markdown/autolink_test.go
+++ utils/markdown/autolink_test.go
@@ -1,7 +1,7 @@
-// // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// // See LICENSE.txt for license information.
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
 
-// package markdown
+package markdown
 
 // import (
 // 	"testing"
