2e3aa53587defdfb77463c6c20d54b62df2618cf
hung <hungbn0112@gmail.com>
2021-01-28 18:22:51 +0700

    Add test for TestCreateTrackPointForRecord

--- api1/track_record_test.go
+++ api1/track_record_test.go
@@ -35,8 +35,14 @@ func TestCreateTrackPointForRecord(t *testing.T) {
 
   trackRecord := th.CreateTrackRecord()
 
-  trackPoint := &model.TrackPoint{
+  targetId := model.NewId()
 
+  trackPoint := &model.TrackPoint{
+    TargetId: targetId,
+    Point: model.GeoPoint{
+      Lat: 12.23,
+      Lng: 44.22,
+    },
   }
   rTrackPoint, resp := th.Client.CreateTrackPointForRecord(trackRecord.Id, trackPoint)
 	CheckBadRequestStatus(t, resp)
@@ -46,6 +52,8 @@ func TestCreateTrackPointForRecord(t *testing.T) {
   trackRecordStarted, resp := th.Client.StartTrackRecord(trackRecord.Id)
   CheckOKStatus(t, resp)
   require.NotNil(t, trackRecordStarted)
+  require.Greater(t, trackRecordStarted.StartAt, int64(0))
+  require.Equal(t, trackRecordStarted.EndAt, int64(0))
 
   // expect we can create track point for record after starting the record
   rTrackPoint, resp = th.Client.CreateTrackPointForRecord(trackRecord.Id, trackPoint)
@@ -56,4 +64,6 @@ func TestCreateTrackPointForRecord(t *testing.T) {
   trackRecordEnd, resp := th.Client.EndTrackRecord(trackRecord.Id)
   CheckOKStatus(t, resp)
   require.NotNil(t, trackRecordEnd)
+  require.Greater(t, trackRecordEnd.StartAt, int64(0))
+  require.Greater(t, trackRecordEnd.EndAt, int64(0))
 }
